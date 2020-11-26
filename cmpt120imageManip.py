cb137c34b16d0c76042eda00113d2bd3ea64a7f0
Connor Goudie <connorgoudie@live.com>
2020-11-26 14:30:18 -0800

    Added basic mode algorithms (cannot be used in app yet)
    copy/pasted from week 9 assignment

--- cmpt120imageManip.py
+++ cmpt120imageManip.py
@@ -6,3 +6,43 @@
 
 import cmpt120imageProj
 import numpy
+
+def invert(pixels) :
+  width = len(pixels)
+  height = len(pixels[0])
+
+  # invert each pixel by subtracting 255 from the color value and taking the
+  # absolute value of it
+  for i in range(width) :
+    for j in range(height) :
+      for k in range(3) :
+        pixels[i][j][k] = abs(pixels[i][j][k] - 255)
+  return pixels
+
+def flipHorizontal(pixels) :
+  width = len(pixels)
+  height = len(pixels[0])
+  halfWidth = int(width / 2)
+
+  # For each column from left edge to the center of the image, swap each pixel
+  # with the one on the opposite side horizontally (-i)
+  for i in range(halfWidth) :
+    for j in range(height) :
+      temp = pixels[i][j]
+      pixels[i][j] = pixels[-i][j]
+      pixels[-i][j] = temp
+  return pixels
+
+def flipVertical(pixels) :
+  width = len(pixels)
+  height = len(pixels[0])
+  halfHeight = int(height / 2)
+
+  # For each column from left edge to the center of the image, swap each pixel
+  # with the one on the opposite side vertically (-i)
+  for i in range(width) :
+    for j in range(halfHeight) :
+      temp = pixels[i][j]
+      pixels[i][j] = pixels[i][-j]
+      pixels[i][-j] = temp
+  return pixels
